437e09680bcf580f12fe0ff1b55db9f2bcbb7e9e
xinyi <xinyi.kao@titansoft.com.sg>
2018-11-17 13:36:18 +0800

    Add new test case and new MyZip method

--- ZipSample/test/ZipTests.cs
+++ ZipSample/test/ZipTests.cs
@@ -43,7 +43,34 @@ namespace ZipSample.test
 		    expected.ToExpectedObject().ShouldEqual(girlAndBoyPairs);
 	    }
 
-        private IEnumerable<Tuple<string, string>> MyZip(IEnumerable<Girl> girls, IEnumerable<Key> keys)
+	    [TestMethod]
+	    public void pair_3_boys_and_5_girls()
+	    {
+		    var girls = Repository.Get5Girls();
+		    var keys = Repository.Get3Keys();
+
+		    var girlAndBoyPairs = MyZip(keys, girls).ToList();
+		    var expected = new List<Tuple<string, string>>
+		    {
+			    Tuple.Create("Joey", "Jean"),
+			    Tuple.Create("Frank", "Mary"),
+			    Tuple.Create("Bob", "Karen"),
+		    };
+
+		    expected.ToExpectedObject().ShouldEqual(girlAndBoyPairs);
+	    }
+
+		private IEnumerable<Tuple<string, string>> MyZip(IEnumerable<Key> keys, IEnumerable<Girl> girls)
+		{
+			var firstEnumerator = keys.GetEnumerator();
+			var secondEnumerator = girls.GetEnumerator();
+			while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
+			{
+				yield return Tuple.Create(firstEnumerator.Current.OwnerBoy.Name, secondEnumerator.Current.Name);
+			}
+		}
+
+		private IEnumerable<Tuple<string, string>> MyZip(IEnumerable<Girl> girls, IEnumerable<Key> keys)
         {
 			var firstEnumerator = girls.GetEnumerator();
 			var secondEnumerator = keys.GetEnumerator();
