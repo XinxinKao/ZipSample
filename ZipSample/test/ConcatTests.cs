091764c7184f5ead4eafc82680403a270d535164
xinyi <xinyi.kao@titansoft.com.sg>
2018-11-17 10:48:33 +0800

    Move class

--- ZipSample/test/ConcatTests.cs
+++ ZipSample/test/ConcatTests.cs
@@ -6,12 +6,6 @@ using System.Linq;
 
 namespace ZipSample.test
 {
-	public class Employee
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
-
 	[TestClass]
     public class ConcatTests
     {
