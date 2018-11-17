faf4a177d13966f08a9f29a9783861e782ad09b8
xinyi <xinyi.kao@titansoft.com.sg>
2018-11-17 15:18:28 +0800

    Reformat

--- ZipSample/test/UnionTests.cs
+++ ZipSample/test/UnionTests.cs
@@ -6,31 +6,31 @@ using System.Linq;
 
 namespace ZipSample.test
 {
-    [TestClass]
-    public class UnionTests
-    {
-        [TestMethod]
-        public void Union_integers()
-        {
-            var first = new List<int> { 1, 3, 5 };
-            var second = new List<int> { 5, 3, 7, 9 };
+	[TestClass]
+	public class UnionTests
+	{
+		[TestMethod]
+		public void Union_integers()
+		{
+			var first = new List<int> { 1, 3, 5 };
+			var second = new List<int> { 5, 3, 7, 9 };
 
-            var expected = new List<int> { 1, 3, 5, 7, 9 };
+			var expected = new List<int> { 1, 3, 5, 7, 9 };
 
-            var actual = first.MyUnion(second).ToList();
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
+			var actual = first.MyUnion(second).ToList();
+			expected.ToExpectedObject().ShouldEqual(actual);
+		}
 
-	    [TestMethod]
-	    public void Union_girls()
-	    {
-		    var first = new List<Girl> { new Girl() { Name = "Amanda" }, new Girl() { Name = "Lucy" } };
-		    var second = new List<Girl> { new Girl() { Name = "Lucy" }, new Girl() { Name = "Xinyi" } };
+		[TestMethod]
+		public void Union_girls()
+		{
+			var first = new List<Girl> { new Girl() { Name = "Amanda" }, new Girl() { Name = "Lucy" } };
+			var second = new List<Girl> { new Girl() { Name = "Lucy" }, new Girl() { Name = "Xinyi" } };
 
-		    var expected = new List<Girl> { new Girl() { Name = "Amanda" }, new Girl() { Name = "Lucy" }, new Girl() { Name = "Xinyi" } };
+			var expected = new List<Girl> { new Girl() { Name = "Amanda" }, new Girl() { Name = "Lucy" }, new Girl() { Name = "Xinyi" } };
 
-		    var actual = first.MyUnion(second, new GirlEqualityComparer()).ToList();
-		    expected.ToExpectedObject().ShouldEqual(actual);
-	    }
-    }
+			var actual = first.MyUnion(second, new GirlEqualityComparer()).ToList();
+			expected.ToExpectedObject().ShouldEqual(actual);
+		}
+	}
 }
